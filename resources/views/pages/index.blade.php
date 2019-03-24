265edf25da92c7092ff92c8aaad9f939075bc2e6
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-03-24 23:12:10 +0600

    Extended the layout from app.blade.php file

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,11 +1,4 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{config('app.name', 'MyBlog')}}</title>
-    </head>
-    <body>
-        <h1>Hello there!!</h1>
-    </body>
-</html>
+@extends('layouts.app')
+@section('content')
+    <h1>Hello there!!</h1>
+@endsection
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -1,11 +1,4 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{config('app.name', 'MyBlog')}}</title>
-    </head>
-    <body>
-        <h1>Services</h1>
-    </body>
-</html>
+@extends('layouts.app')
+@section('content')
+    <h1>Services We provide</h1>
+@endsection
