08fc6f7beb26d7acb1c6360826fa29a481b8872f
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-04-06 00:54:15 +0600

    Added the login and register button

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,4 +1,8 @@
 @extends('layouts.app')
 @section('content')
-    <h1>{{$title}}</h1>
+<div class="jumbotron text-center">
+    <h1>Welcome</h1>
+    <p>This is just a test paragraph</p>
+    <p><a href="/login" class="btn btn-primary btn-lg" role="button">Login</a> <a href="/register" class="btn btn-success btn-lg" role="button">Register</a></p>
+</div>
 @endsection
