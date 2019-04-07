aea6cae0f5421299dd2cb53e909b2dc02246e3ef
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-04-07 21:23:04 +0600

    Updated the model Post.php and gave them reference

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // Table Name 
+    protected $table = 'posts';
+
+    // primary key 
+    public $primaryKey = 'id';
+    // Timestamps
+    public $timestamps = true;
 }
