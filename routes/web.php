8685adcec289ce6b40f8f4fcd44ec4bfea35f664
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-05-24 17:05:52 +0600

    Updated home to dashboard had to change a lot of things

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,4 @@ Route::resource('posts', 'PostsController');
 // });
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/dashboard', 'DashboardController@index');
