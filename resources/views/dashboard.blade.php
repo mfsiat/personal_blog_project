5ce1436c2c4b3efe58a39ea5f262a8f3feb93732
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-06-26 22:36:13 +0600

    repaired the dashboard dropdown on navbar and some bootstrap class

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,20 +15,24 @@
                     @endif
                     <a href="/posts/create" class="btn btn-primary">Create Post</a>
                     <h3>Your blog post</h3>
-                    <table class="table table-stipped">
-                        <tr>
-                            <th>Title</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                        @foreach ($posts as $post)
-                        <tr>
-                            <th>{{$post->title}}</th>
-                            <th><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></th>
-                            <th></th>
-                        </tr>                            
-                        @endforeach
-                    </table>
+                    @if (count($posts) > 0)
+                        <table class="table table-stipped">
+                            <tr>
+                                <th>Title</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            @foreach ($posts as $post)
+                            <tr>
+                                <th>{{$post->title}}</th>
+                                <th><a href="/posts/{{$post->id}}/edit" class="btn btn-success">Edit</a></th>
+                                <th></th>
+                            </tr>                            
+                            @endforeach
+                        </table>
+                    @else
+                        <p>You Have No Post</p>                            
+                    @endif
                 </div>
             </div>
         </div>
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -58,7 +58,8 @@
                         </div> --}}
 
             <ul class="dropdown-menu" role="menu">
-                <li><a href="/dashboard">Dashboard</a></li>
+                <li><a href="/dashboard" class="dropdown-item">Dashboard</a></li>
+                {{-- <li><a href="/dashboard" class="btn btn-success">Dashboard</a></li> --}}
                 <li><a class="dropdown-item" href="{{ route('logout') }}"onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
                             
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>    
